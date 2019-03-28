c6b2f0759d3ac0e71456112a38808530c15a3b69
fatherofikura <fatherofikura0107@gmail.com>
2019-03-29 07:34:32 +0900

    [Vue.jsとFirebaseで作るミニWebサービス] 40%(457/1174)

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button>Googleアカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcom to MyMarkdown"
+    };
+  }
+};
+</script>
